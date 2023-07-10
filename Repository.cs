ef2964c007bee27c6875e17575bb93c933189b4c
TeoLiss <fedya199797@gmail.com>
2023-07-10 19:15:13 +0300

    Добавьте файлы проекта.

---
+++ Repository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7_1
+{
+    internal class Repository
+    {
+        public Worker[] GetAllWorkers()
+        {
+            // здесь происходит чтение из файла
+            // и возврат массива считанных экземпляров
+        }
+
+        public Worker GetWorkerById(int id)
+        {
+            // происходит чтение из файла, возвращается Worker
+            // с запрашиваемым ID
+        }
+
+        public void DeleteWorker(int id)
+        {
+            // считывается файл, находится нужный Worker
+            // происходит запись в файл всех Worker,
+            // кроме удаляемого
+        }
+
+        public void AddWorker(Worker worker)
+        {
+            // присваиваем worker уникальный ID,
+            // дописываем нового worker в файл
+        }
+
+        public Worker[] GetWorkersBetweenTwoDates(DateTime dateFrom, DateTime dateTo)
+        {
+            // здесь происходит чтение из файла
+            // фильтрация нужных записей
+            // и возврат массива считанных экземпляров
+        }
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7_1
+{
+    struct Worker
+    {
+    public Worker(int ID, DateTime Createdate, string FIO, int Age, int Height, DateTime Dateofbirth, string Placeofbirth)
+        {
+
+            ///summary> Пояснения переменных
+            /// <param name = "ID">  Уникальный номер сотрудника</param>
+            /// <param name = "Createdate"> Дата и время создания записи</param> 
+            /// <parame name = "FIO"> ФИО сотрудника </param> 
+            /// <param name = "Age"> возраст <param>
+            /// <param name = "Height"> рост(очень дотошный работодатель...) </param>
+            ///<param name = "Dateofbirth"> Дата рождения </param> 
+            ///<param name = "Placeofbirth"> Место рождения </param> 
+
+            this.ID = ID;
+            this.Createdate = Createdate;
+            this.FIO = FIO;
+            this.Age = Age;
+            this.Height = Height;
+            this.Dateofbirth = Dateofbirth;
+            this.Placeofbirth = Placeofbirth; 
+        }       
+     public string Print ()
+        {
+            return $"{this.ID,15}{this.Createdate,15}{this.FIO,15}{this.Age,15}{this.Height,15}{this.Dateofbirth,15}{this.Placeofbirth}";
+        }
+
+     public string ID { get { return this.ID; } set { this.ID = value; } }  
+     public DateTime Createdate { get { return this.Createdate; } set { this.Createdate = value; } }
+     public string FIO { get { return this.FIO; } set { this.FIO = value; } }   
+     public int Age { get { return this.Age; } set { this.Age = value; } }
+     public int Height { get { return this.Height; } set { this.Height = value; } } 
+     public DateTime Dateofbirth { get {  return this.Dateofbirth; } set { this.Dateofbirth = value; } }
+     public string Placeofbirth { get { return this.Placeofbirth; } set { this.Placeofbirth = value; } }
+
+        private int ID;
+        private DateTime Createdate;
+        private string FIO;
+        private int Age;
+        private int Height;
+        private DateTime Dateofbirth;
+        private string Placeofbirth;
+    }
+}
